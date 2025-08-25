92069af74f7bfb8c085f69e8f8709b645db3d5a5
JuS <wntjd0643@gmail.com>
2025-08-26 00:49:29 +0900

    Create AdminController.java
    admin관련 controller 추가.

---
+++ web/src/main/java/controller/AdminController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("*.admin")
+public class AdminController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            //region 임시적 모든 dao 선언
+            BoardCategoryDAO boardCategoryDAO = BoardCategoryDAO.getInstance();
+            BoardDAO boardDAO = BoardDAO.getInstance();
+            BoardFileDAO boardFileDAO = BoardFileDAO.getInstance();
+            ReplyDAO replyDAO = ReplyDAO.getInstance();
+            GameDAO gameDAO = GameDAO.getInstance();
+            GameReplyDAO gameReplyDAO = GameReplyDAO.getInstance();
+            GameScoreDAO gameScoreDAO = GameScoreDAO.getInstance();
+            BlackListDAO blackListDAO = BlackListDAO.getInstance();
+            MemberDAO memberDAO = MemberDAO.getInstance();
+            MemberGameTierDAO memberGameTierDAO = MemberGameTierDAO.getInstance();
+            MemberProfileFileDAO memberProfileFileDAO = MemberProfileFileDAO.getInstance();
+            //endregion
+
+            //TODO: admin권한 확인
+            if(false) {
+                
+            }
+          
+            String cmd = request.getRequestURI();
+
+            switch (cmd) {
+                // 행위 + 자원 (e.g, /getMemberList.member로 작성 요망)
+                //TODO: admin 관련 기능
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            response.sendRedirect("/error.jsp");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
