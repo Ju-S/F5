b5c9b888107fed76206790f046a64bae971da3df
pjw <jann2@naver.com>
2025-08-29 17:09:25 +0900

    index.jsp /signin.jsp /login.jsp/sidenavibar 1차 완성 + 네비바 링크 구현 + 네비바에 이미지 넣기중 ~(색상변경 및 칼라변경)
    0829 지원
    
    보완 필요:
    0. 업데이트 된 commons 반영 필요
    1. 모든 버튼 호버 했을때 효과 지정
    2. 인덱스에서 게임div 클릭했을 때 보더라인 빛나는 것처럼
    3. 컨트롤러를 통해서 경로 지정
    4. 로그인 페이지 js 완성
    5. 회원가입 페이지 js 완성
    6. js파일들 마지막에 리팩토링 하기
    
     0829지원

--- web/src/main/webapp/common/sidenavi/SideNavi.js
+++ web/src/main/webapp/common/sidenavi/SideNavi.js
@@ -2,66 +2,96 @@ $(document).ready(function () {
 
 
 // 버튼 열려있는지에 대한 함수
-const menuState = {
-    gamebtn: false,
-    boardbtn: false,
-    mypagebtn: false
-};
+    const menuState = {
+        gamebtn: false,
+        boardbtn: false,
+        mypagebtn: false
+    };
 
 //아이콘 이미지 배열로 넘기기
-let gameIcons = [
-        { src: "/common/sidenavi/sideNaviIcon/game1.svg", link: "/game1" },
-        { src: "/common/sidenavi/sideNaviIcon/game2.svg", link: "/game2" },
-        { src: "/common/sidenavi/sideNaviIcon/game3.svg", link: "/game3" },
-        { src: "/common/sidenavi/sideNaviIcon/game4.svg", link: "/game4" }
+    let gameIcons = [
+        {src: "/common/sidenavi/sideNaviIcon/game1.svg", link: "/game1"},
+        {src: "/common/sidenavi/sideNaviIcon/game2.svg", link: "/game2"},
+        {src: "/common/sidenavi/sideNaviIcon/game3.svg", link: "/game3"},
+        {src: "/common/sidenavi/sideNaviIcon/game4.svg", link: "/game4"}
     ];
-let boardIcons = [
-        { src: "/common/sidenavi/sideNaviIcon/Group.svg", link: "/game/allList.jsp" },
-        { src: "/common/sidenavi/sideNaviIcon/game1.svg", link: "/game/game1List.jsp" },
-        { src: "/common/sidenavi/sideNaviIcon/game2.svg", link: "/game/game2List.jsp" },
-        { src: "/common/sidenavi/sideNaviIcon/game3.svg", link: "/game/game3List.jsp" },
-        { src: "/common/sidenavi/sideNaviIcon/game4.svg", link: "/game/game4List.jsp" }
+    let boardIcons = [
+        {src: "/common/sidenavi/sideNaviIcon/Group.svg", link: "/game/allList.jsp"},
+        {src: "/common/sidenavi/sideNaviIcon/game1.svg", link: "/game/game1List.jsp"},
+        {src: "/common/sidenavi/sideNaviIcon/game2.svg", link: "/game/game2List.jsp"},
+        {src: "/common/sidenavi/sideNaviIcon/game3.svg", link: "/game/game3List.jsp"},
+        {src: "/common/sidenavi/sideNaviIcon/game4.svg", link: "/game/game4List.jsp"}
     ];
-let mypageIcons = [
-    { src: "/common/sidenavi/sideNaviIcon/game1.svg", link: "/myPage1" },
-    { src: "/common/sidenavi/sideNaviIcon/game2.svg", link: "/myPage2" }
-];
+    let mypageIcons = [
+        {src: "/common/sidenavi/sideNaviIcon/game1.svg", link: "/myPage1"},
+        {src: "/common/sidenavi/sideNaviIcon/game2.svg", link: "/myPage2"}
+    ];
 
 
-//공통 마우스오버 이벤트 함수
-    function mouseoverHandle(buttonId){
-        //맨 윗 아이콘에대하여 하얀색 아이콘으로 변경
+// 공통 클릭 이벤트 함수
+    function clickHandle(buttonId, iconsArr) {
         const $btn = $(`#${buttonId}`);
-        const $firstIcon = $btn.find(".main-icon img");
-        if(buttonId==="gamebtn"){//게임버튼 눌렀을 때
-            $firstIcon.attr("src","/common/sidenavi/sideNaviIcon/main_game.svg");
 
-        }else if(buttonId==="boardbtn"){
-            $firstIcon.attr("src","/common/sidenavi/sideNaviIcon/main_massge.svg");
+        if (!menuState[buttonId]) {// 버튼 열려있지 않으면
 
-        }else if(buttonId==="mypagebtn"){
-            $firstIcon.attr("src","/common/sidenavi/sideNaviIcon/main_mypage.svg");
+            closeAllMenus(); // 일단 다른 메뉴 다 닫고
+            menuState[buttonId] = true;//현재것만 true로 변경
+
+            //맨 윗 아이콘에대하여 하얀색 아이콘으로 변경
+            const $firstIcon = $btn.find(".main-icon img");
+            if (buttonId === "gamebtn") {//게임버튼 눌렀을 때
+                $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/main_game.svg");
+
+            } else if (buttonId === "boardbtn") {
+                $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/main_massge.svg");
+
+            } else if (buttonId === "mypagebtn") {
+                $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/main_mypage.svg");
+            }
+
+
+            //서브메뉴 추가
+            const $submenu = $("<div>").addClass("submenu");
+            for (let icon of iconsArr) {
+                const $img = $("<img>").attr("src", icon.src);
+                const $div = $("<div>").addClass("icon").append($img);
+                const $iconA = $("<a>").attr("href", icon.link).append($div);
+                $submenu.append($iconA);
+            }
+            $btn.append($submenu);
+
+            //0초 뒤에 open 클래스 주기: submenu크기를 안의 내용물에 맞춰 키운다(max-height)
+            setTimeout(() => {
+                $submenu.addClass("open");
+            }, 100);
+
+        } else { // 버튼 열려있으면 않으면
+            menuState[buttonId] = false;
+            closeBtn($btn);
         }
     }
-//마우스오버 이벤트 바인딩
-    $("#gamebtn").on("mouseover", function (e) {
+
+// 버튼 클릭 이벤트 바인딩
+    $("#gamebtn").on("click", function (e) {
         e.stopPropagation();
-        mouseoverHandle("gamebtn",gameIcons);
+        clickHandle("gamebtn", gameIcons);
     });
-    $("#boardbtn").on("mouseover", function (e) {
+    $("#boardbtn").on("click", function (e) {
         e.stopPropagation();
-        mouseoverHandle("boardbtn",boardIcons);
+        clickHandle("boardbtn", boardIcons);
     });
-    $("#mypagebtn").on("mouseover", function (e) {
+    $("#mypagebtn").on("click", function (e) {
         e.stopPropagation();
-        mouseoverHandle("mypagebtn",mypageIcons);
+        clickHandle("mypagebtn", mypageIcons);
     });
 
 
-//공통 마우스리브 이벤트 함수
-    function mouseleaveHandle(buttonId){
-        // 아이콘 원래 이미지로 복구
-        const $btn = $(`#${buttonId}`);
+// 닫기 함수
+    function closeBtn($btn) {
+        const $submenu = $btn.find(".submenu");
+        $submenu.removeClass("open");
+
+
         const $firstIcon = $btn.find(".main-icon img");
         if ($btn.attr("id") === "gamebtn") {
             $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/main_game_color.svg");
@@ -70,108 +100,15 @@ let mypageIcons = [
         } else if ($btn.attr("id") === "mypagebtn") {
             $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/main_mypage_color.svg");
         }
-    }
-//마우스리브 이벤트 바인딩
-    $("#gamebtn").on("mouseleave", function (e) {
-        e.stopPropagation();
-        mouseleaveHandle("gamebtn",gameIcons);
-    });
-    $("#boardbtn").on("mouseleave", function (e) {
-        e.stopPropagation();
-        mouseleaveHandle("boardbtn",boardIcons);
-    });
-    $("#mypagebtn").on("mouseleave", function (e) {
-        e.stopPropagation();
-        mouseleaveHandle("mypagebtn",mypageIcons);
-    });
 
 
-
-
-// 공통 클릭 이벤트 함수
-function clickHandle(buttonId, iconsArr) {
-    const $btn = $(`#${buttonId}`);
-
-    if (!menuState[buttonId]) {// 버튼 열려있지 않으면
-
-        closeAllMenus(); // 일단 다른 메뉴 다 닫고
-        menuState[buttonId] = true;//현재것만 true로 변경
-
-        //맨 윗 아이콘에대하여 하얀색 아이콘으로 변경
-        const $firstIcon = $btn.find(".main-icon img");
-        if(buttonId==="gamebtn"){//게임버튼 눌렀을 때
-            $firstIcon.attr("src","/common/sidenavi/sideNaviIcon/main_game.svg");
-
-        }else if(buttonId==="boardbtn"){
-            $firstIcon.attr("src","/common/sidenavi/sideNaviIcon/main_massge.svg");
-
-        }else if(buttonId==="mypagebtn"){
-            $firstIcon.attr("src","/common/sidenavi/sideNaviIcon/main_mypage.svg");
-        }
-
-
-        //서브메뉴 추가
-        const $submenu = $("<div>").addClass("submenu");
-        for (let icon of iconsArr) {
-            const $img = $("<img>").attr("src", icon.src);
-            const $div =$("<div>").addClass("icon").append($img);
-            const $iconA = $("<a>").attr("href", icon.link).append($div);
-            $submenu.append($iconA);
-        }
-        $btn.append($submenu);
-
-        //0초 뒤에 open 클래스 주기: submenu크기를 안의 내용물에 맞춰 키운다(max-height)
         setTimeout(() => {
-            $submenu.addClass("open");
-        }, 100);
-
-    } else { // 버튼 열려있으면 않으면
-        menuState[buttonId] = false;
-        closeBtn($btn);
-    }
-}
-
-
-// 버튼 클릭 이벤트 바인딩
-    $("#gamebtn").on("click", function (e) {
-        e.stopPropagation();
-        clickHandle("gamebtn",gameIcons);
-    });
-    $("#boardbtn").on("click", function (e) {
-        e.stopPropagation();
-        clickHandle("boardbtn",boardIcons);
-    });
-    $("#mypagebtn").on("click", function (e) {
-        e.stopPropagation();
-        clickHandle("mypagebtn",mypageIcons);
-    });
-
-
-
-
-// 닫기 함수
-function closeBtn($btn) {
-    const $submenu = $btn.find(".submenu");
-    $submenu.removeClass("open");
-
-
-    const $firstIcon = $btn.find(".main-icon img");
-    if ($btn.attr("id") === "gamebtn") {
-        $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/main_game_color.svg");
-    } else if ($btn.attr("id") === "boardbtn") {
-        $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/main_massge_color.svg");
-    } else if ($btn.attr("id") === "mypagebtn") {
-        $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/main_mypage_color.svg");
+            const $firstIcon = $btn.find(".icon:nth-child(1)");//맨 윗 아이콘에대하여
+            $firstIcon.css("margin-bottom", "0px");//마진0
+            $submenu.remove();
+        }, 200);
     }
 
-
-
-    setTimeout(() => {
-        const $firstIcon = $btn.find(".icon:nth-child(1)");//맨 윗 아이콘에대하여
-        $firstIcon.css("margin-bottom","0px");//마진0
-        $submenu.remove();
-    }, 200);
-}
 // 모든 메뉴 닫기
     function closeAllMenus() {
         Object.keys(menuState).forEach(id => {
@@ -182,17 +119,59 @@ function closeBtn($btn) {
         });
     }
 
+// 로그아웃 버튼 마우스 오버
+    $("#logoutbtn").on("mouseover", function () {
+        //이미지 태그찾기
+        const $firstIcon = $(this).find("img");
+        $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/logout.svg");
+    })
+// 로그아웃 버튼 마우스 리브
+    $("#logoutbtn").on("mouseleave", function () {
+        //이미지 태그찾기
+        const $firstIcon = $(this).find("img");
+        $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/logout_color.svg");
+    })
+// 로그아웃 버튼 클릭
+    $("#logoutbtn").on("click", function () {
+        //이미지 태그찾기
+        const $firstIcon = $(this).find("img");
+        $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/logout.svg");
+    })
+
+
+
+// 홈 버튼 마우스 오버
+    $("#homebtn").on("mouseover", function () {
+        //이미지 태그찾기
+        const $firstIcon = $(this).find("img");
+        $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/Home-1.svg");
+    })
+// 홈 버튼 마우스 리브
+    $("#homebtn").on("mouseleave", function () {
+        //이미지 태그찾기
+        const $firstIcon = $(this).find("img");
+        $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/home_color.svg");
+    })
+// 홈 버튼 버튼 클릭
+    $("#homebtn").on("click", function () {
+        //이미지 태그찾기
+        const $firstIcon = $(this).find("img");
+        $firstIcon.attr("src", "/common/sidenavi/sideNaviIcon/Home-1.svg");
+    })
+
+
+
 
 // 문서 클릭 시 메뉴 닫기
-$(document).on("click", function (e) {
-    if (
-        !$(e.target).closest("#gamebtn").length &&
-        !$(e.target).closest("#boardbtn").length &&
-        !$(e.target).closest("#mypagebtn").length
-    ) {
-        closeAllMenus();
-    }
-});
+    $(document).on("click", function (e) {
+        if (
+            !$(e.target).closest("#gamebtn").length &&
+            !$(e.target).closest("#boardbtn").length &&
+            !$(e.target).closest("#mypagebtn").length
+        ) {
+            closeAllMenus();
+        }
+    });
 
 
 });
